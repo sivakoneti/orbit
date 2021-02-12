b48db682b9403c11f475b632ff169caaf7450111
mainframev <vgenaev@gmail.com>
2021-02-13 02:10:23 +0500

    feat(eslint-plugin-orbit): default-theme rule

--- packages/eslint-plugin-orbit-components/src/index.ts
+++ packages/eslint-plugin-orbit-components/src/index.ts
@@ -1,10 +1,12 @@
 import buttonHasTitle from "./rules/buttonHasTitle";
 import unnecessaryText from "./rules/unnecessaryText";
 import recommended from "./configs/recommended";
+import defaultTheme from "./rules/defaultTheme";
 
 export const rules = {
   "button-has-title": buttonHasTitle,
   "unnecessary-text": unnecessaryText,
+  "default-theme": defaultTheme,
 };
 
 export const configs = {
